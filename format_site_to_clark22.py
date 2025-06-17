9858c5fadc8a1940a2a9d4a00ce97e35fb1ae0ff
Tom B <tsb42@cam.ac.uk>
2025-06-17 10:29:51 +0100

    Working version to fetch Brakes items and impacts

---
+++ format_site_to_clark22.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 07:52:46 2024
+
+@author: Thomas Ball
+"""
+
+import pandas as pd
+import os
+import urllib
+from tqdm import tqdm
+import tldextract
+
+OVERWRITE = True
+SITE_URL = "https://www.brake.co.uk/"
+CLARK_PATH = os.path.join("dat", "clark_data")
+sitename = tldextract.extract(SITE_URL).domain
+
+if not os.path.isdir(CLARK_PATH):
+    import requests
+    import zipfile
+    import io
+    clark_dat_url = "https://ora.ox.ac.uk/objects/uuid:4ad0b594-3e81-4e61-aefc-5d869c799a87/files/sst74cr71b"    
+    response = requests.get(clark_dat_url)
+    response.raise_for_status()
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
+        zip_file.extractall(CLARK_PATH)
+    
+cache_data_dir = os.path.join("dat", "site_data")
+cache_data_name = f"{sitename}_site_data.csv"
+cache_data_path = os.path.join(cache_data_dir, cache_data_name)
+
+cats_df = pd.read_csv(os.path.join(CLARK_PATH, "Clark_et_al_2022_PNAS_SM", 
+                                   "foodDB_dat", "categories anonymised.csv"), 
+                      encoding = "latin-1")
+prods_df = pd.read_csv(os.path.join(CLARK_PATH, "Clark_et_al_2022_PNAS_SM",
+                        "foodDB_dat", "products anonymised.csv"), 
+                       encoding = "latin-1", low_memory=False)
+
+df = pd.read_csv(cache_data_path, index_col = 0)
+
+cdf = pd.DataFrame(columns = cats_df.columns.to_list() + ["shop_uid"])
+pdf = pd.DataFrame(columns = prods_df.columns)
+
+pdf_cols = {'Energy (kcal)' : "energy_per_100",
+            #'Energy (KJ)' : None,
+            'Protein (g)' : "protein_per_100", 
+            'Carbohydrates (g)' : "carbohydrate_per_100",
+            'Of which sugars (g)' : "sugar_per_100",
+            'Fat (g)' : "fat_per_100",
+            'Of which saturates (g)' : "saturates_per_100", 
+            'Fibre (g)' : "fibre_per_100",
+            'Salt (g)' : "salt_per_100",
+            "Ingredients_string" : "ingredients_text",
+            'Pack size_string' : "pack_size_string",
+            " items_in_pack" : "items_in_pack",
+            "item_mass_g" : "item_mass_g",
+            "portions_in_pack" : "portions_in_pack"
+            }
+
+for r, (idx, row) in tqdm(enumerate(df.iterrows()), total=len(df), desc="Formatting.."):
+    if not pd.isnull(row.Nutrition_string):
+        row_id = len(pdf)
+        
+        shop_location = {"product_id":      str(row.iloc[1]) + tldextract.extract(SITE_URL).domain, # to make sure nothing overlaps with existing food-db data
+                        "name" :            row["Product name_string"],
+                        "main_category" :   row.iloc[7],
+                        "department" :      row.iloc[6],
+                        "aisle" :           row.iloc[5],
+                        "shelf" :           row.iloc[4],
+                        "shop_uid":        row.iloc[1]}
+        
+        cdf.loc[row_id] = pd.Series(shop_location)
+        
+        pdf.loc[row_id, "product_id"] = str(row.iloc[1]) + tldextract.extract(SITE_URL).domain # this may break things down the line..
+        pdf.loc[row_id, "product_name"] = row["Product name_string"]
+        pdf.loc[row_id, "url"] = row.URL
+        pdf.loc[row_id, "ingredients_text"] = row.Ingredients_string
+        pdf.loc[row_id, "Retailer"] = tldextract.extract(SITE_URL).domain
+        pdf.loc[row_id, "shop_uid"] = row.iloc[1]
+        
+        for item in row.index:
+            if item in pdf_cols.keys():
+                pdf.loc[row_id, pdf_cols[item]] = row[item]
+        
+
+products_out_path = os.path.join(cache_data_dir, f"products_{sitename}.csv")
+pdf.to_csv(products_out_path, index=False)
+categories_out_path = os.path.join(cache_data_dir, f"categories_{sitename}.csv")
+cdf.to_csv(categories_out_path, index=False)
+
+# prods_df_concat = pd.concat([pdf, prods_df])
+# cats_df_concat = pd.concat([cdf, cats_df])
+
+# prods_df_concat.to_csv(os.path.join("clark_mod", "products_concat.csv"))
+# cats_df_concat.to_csv(os.path.join("clark_mod", "categories_concat.csv"))
---
+++ run_clark22.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 09:31:59 2025
+
+This is because I dont speak R very well and it was quicker to control the
+inputs to the clark scripts here than in R. 
+
+@author: Thomas Ball
+"""
+
+import os
+import rpy2.robjects as ro
+import tldextract
+
+SITE_URL = "https://www.brake.co.uk/sitemap.xml"
+dat_path = "dat"
+
+
+site = tldextract.extract(SITE_URL).domain
+
+# Create args
+args = {"arg1" : os.path.abspath(os.getcwd()),
+        "arg2" : os.path.abspath(os.path.join(dat_path, "clark_mod_outputs", site)),
+        "arg3" : os.path.abspath(os.path.join(dat_path, "site_data", f"products_{site}.csv")),
+        "arg4" : os.path.abspath(os.path.join(dat_path, "site_data", f"categories_{site}.csv")),
+        
+        # constant
+        "arg5" : os.path.abspath(os.path.join(dat_path, "clark_mod", "0.0_Functions_Estimating_Composition_22January2022.R")),
+        "arg6" : os.path.abspath(os.path.join(dat_path, "clark_data", "Clark_et_al_2022_PNAS_SM", "Data Inputs"))
+
+        }
+
+if not os.path.isdir(os.path.join(dat_path, "clark_mod_outputs", site)): 
+    os.makedirs(os.path.join(dat_path, "clark_mod_outputs", site), exist_ok=True)
+   
+packages = ["plyr", "dplyr", "readr", "stringr", "stringi", "reshape2", "dismo"]
+for package in packages:
+    ro.r(f'if (!requireNamespace("{package}", quietly = TRUE)) install.packages("{package}", repos="http://cran.r-project.org")')
+
+estimating_comps_script_path = os.path.abspath(os.path.join(
+    "dat", "clark_mod", "1.0_Estimating_Composition_21January2022.R"))
+
+# create args for script
+for arg, arg_val in args.items():
+    ro.r.assign(arg, arg_val)
+
+if os.path.isfile(estimating_comps_script_path):
+    ro.r.source(estimating_comps_script_path)
+else:
+    print(f"Can't find {estimating_comps_script_path}")
